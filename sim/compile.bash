9174f49bc2914f453d1f385e5b31b7ce6f5c06e0
Loreno Carrano <lorenzoc7297@gmail.com>
2021-10-16 14:22:01 +0200

    Modified Verilog tb file

---
+++ sim/compile.bash
@@ -0,0 +1,4 @@
+vcom -work work -2002 -explicit -vopt /home/isa21_2021_2022/labs/lab1/ISAL1/srcfd.vhd
+vcom -work work -2002 -explicit -vopt /home/isa21_2021_2022/labs/lab1/ISAL1/register.vhd
+vcom -work work -2002 -explicit -vopt /home/isa21_2021_2022/labs/lab1/ISAL1/filter.vhd
+vcom -work work -2002 -explicit -vopt /home/isa21_2021_2022/labs/lab1/ISAL1/FIR_STAGE.vhd
--- tb/tb_fir.v
+++ tb/tb_fir.v
@@ -4,13 +4,21 @@ module tb_fir ();
 
    wire CLK_i;
    wire RST_n_i;
-   wire [15:0] DIN_i;
+   //originally was [15:00]
+   wire [7:0] DIN_i;
    wire VIN_i;
-   wire [15:0] H0_i;
-   wire [15:0] H1_i;
-   wire [15:0] H2_i;
-   wire [15:0] H3_i;
-   wire [15:0] DOUT_i;
+   wire [7:0] H0_i; 
+   wire [7:0] H1_i;
+   wire [7:0] H2_i;
+   wire [7:0] H3_i;
+   wire [7:0] H4_i; 
+   wire [7:0] H5_i;
+   wire [7:0] H6_i;
+   wire [7:0] H7_i;
+   wire [7:0] H8_i; 
+   wire [7:0] H9_i;
+   wire [7:0] H10_i;
+   wire [7:0] DOUT_i;
    wire VOUT_i;
    wire END_SIM_i;
 
@@ -26,18 +34,32 @@ module tb_fir ();
 		 .H1(H1_i),
 		 .H2(H2_i),
 		 .H3(H3_i),
+	     .H4(H4_i),
+		 .H5(H5_i),
+		 .H6(H6_i),
+		 .H7(H7_i),
+         .H8(H8_i),
+		 .H9(H9_i),
+		 .H10(H10_i),
 		 .END_SIM(END_SIM_i));
 
    myfir UUT(.CLK(CLK_i),
 	     .RST_n(RST_n_i),
 	     .DIN(DIN_i),
-             .VIN(VIN_i),
+         .VIN(VIN_i),
 	     .H0(H0_i),
-	     .H1(H1_i),
-	     .H2(H2_i),
-	     .H3(H3_i),
-             .DOUT(DOUT_i),
-             .VOUT(VOUT_i));
+		 .H1(H1_i),
+		 .H2(H2_i),
+		 .H3(H3_i),
+	     .H4(H4_i),
+		 .H5(H5_i),
+		 .H6(H6_i),
+		 .H7(H7_i),
+         .H8(H8_i),
+		 .H9(H9_i),
+		 .H10(H10_i),
+         .DOUT(DOUT_i),
+         .VOUT(VOUT_i));
 
    data_sink DS(.CLK(CLK_i),
 		.RST_n(RST_n_i),
