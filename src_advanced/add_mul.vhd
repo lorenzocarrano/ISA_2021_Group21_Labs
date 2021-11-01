8afaf9dc89a5cf4a9c3eb995329f87f98cfbf30d
stefanomoncalvo <91539126+stefanomoncalvo@users.noreply.github.com>
2021-11-01 12:00:58 +0100

    pipeline 1 testing

--- src_advanced/add_mul.vhd
+++ src_advanced/add_mul.vhd
@@ -7,7 +7,8 @@ use ieee.std_logic_textio.all;
 
 entity MUL_ADD is
     generic (
-        NBIT :		integer := 8);
+        NBIT :		integer := 8;
+        N_REG:      integer :=0);
     port (
       VIN, RST_n, CLK: in std_logic;
       DIN_M, DIN_A : in  signed(NBIT-1 downto 0);
@@ -31,6 +32,19 @@ architecture ARCH of MUL_ADD is
     
     end component;
 
+    component FD is
+
+        Port (	D:	In	std_logic;
+                CK:	In	std_logic;
+                RESET:	In	std_logic;
+                ENABLE: In std_logic;	
+                Q:	Out	std_logic);
+    
+    end component;
+
+    type internalSignal is array(0 to N_REG + 1) of signed(7 downto 0); 
+    signal MULT_REG: internalSignal;
+    signal VIN_pipeline : std_logic_vector(N_REG + 1 downto 0);
 	signal mult_partial: signed(15 downto 0);
     signal mul_reg: signed(7 downto 0);
 
@@ -38,8 +52,17 @@ begin
 
     mult_partial <= DIN_M*C;
 
-    reg_pipeline : REG port map (mult_partial(15 downto 8), CLK, RST_n, VIN, mul_reg);
+    MULT_REG(0) <= mult_partial(15 downto 8);
 
-	DOUT <= mul_reg + DIN_A;
+    VIN_pipeline(0) <= VIN;
+
+    --reg_pipeline : REG port map (mult_partial(15 downto 8), CLK, RST_n, VIN, mul_reg);
+    regs_pipeline: for i in 0 to N_REG Generate
+        fd_i : FD Port Map(VIN_pipeline(i), CLK, RST_N, '1', VIN_pipeline(i+1));
+        reg_i: REG Port Map(MULT_REG(i), CLK, RST_n, VIN_pipeline(i), MULT_REG(i+1));
+    end Generate;
+
+
+	DOUT <= MULT_REG(N_REG+1) + DIN_A;
 
 end ARCH;
--- src_advanced/stage.vhd
+++ src_advanced/stage.vhd
@@ -20,7 +20,8 @@ architecture ARCH of STAGE is
 
     component MUL_ADD is
         generic (
-            NBIT :		integer := 8);
+            NBIT :		integer := 8;
+            N_REG:      integer :=0);
         port (
             VIN, RST_n, CLK : in std_logic;
             DIN_M, DIN_A : in  signed(NBIT-1 downto 0);
@@ -42,8 +43,19 @@ architecture ARCH of STAGE is
     
     end component;
 
+    component FD is
+
+        Port (	D:	In	std_logic;
+                CK:	In	std_logic;
+                RESET:	In	std_logic;
+                ENABLE: In std_logic;	
+                Q:	Out	std_logic);
+    
+    end component;
+
 	signal mult_partial: signed(15 downto 0);
-    signal DIN_0_sig, DIN_1_sig, DIN_2_sig, DIN_3_sig, DIN_4_sig, DIN_5_sig, DIN_6_sig, DIN_7_sig, DIN_8_sig, DIN_9_sig, DIN_10_sig: signed(7 downto 0);
+    signal DIN_0_sig, DIN_1_sig, DIN_2_sig, DIN_3_sig, DIN_4_sig, DIN_5_sig, DIN_6_sig, DIN_7_sig, DIN_8_sig, DIN_9_sig, DIN_10_sig, DIN_5_sig_reg: signed(7 downto 0);
+    signal VIN_pipeline: std_logic;
 
 begin
 
@@ -51,16 +63,19 @@ begin
     reg_init : REG port map (mult_partial(15 downto 8), CLK, RST_n, VIN, DIN_0_sig);
     --DIN_0_sig <= mult_partial(15 downto 8); 
 
-    stage_1 : MUL_ADD port map (VIN, RST_n, CLK, DIN_1, DIN_0_sig, C_1, DIN_1_sig);
-    stage_2 : MUL_ADD port map (VIN, RST_n, CLK, DIN_2, DIN_1_sig, C_2, DIN_2_sig);
-    stage_3 : MUL_ADD port map (VIN, RST_n, CLK, DIN_3, DIN_2_sig, C_3, DIN_3_sig);
-    stage_4 : MUL_ADD port map (VIN, RST_n, CLK, DIN_4, DIN_3_sig, C_4, DIN_4_sig);
-    stage_5 : MUL_ADD port map (VIN, RST_n, CLK, DIN_5, DIN_4_sig, C_5, DIN_5_sig);
-    stage_6 : MUL_ADD port map (VIN, RST_n, CLK, DIN_6, DIN_5_sig, C_6, DIN_6_sig);
-    stage_7 : MUL_ADD port map (VIN, RST_n, CLK, DIN_7, DIN_6_sig, C_7, DIN_7_sig);
-    stage_8 : MUL_ADD port map (VIN, RST_n, CLK, DIN_8, DIN_7_sig, C_8, DIN_8_sig);
-    stage_9 : MUL_ADD port map (VIN, RST_n, CLK, DIN_9, DIN_8_sig, C_9, DIN_9_sig);
-    stage_10 : MUL_ADD port map (VIN, RST_n, CLK, DIN_10, DIN_9_sig, C_10, DIN_10_sig);
+    fd_i : FD Port Map(VIN, CLK, RST_N, '1', VIN_pipeline);
+
+    stage_1 : MUL_ADD generic map (NBIT => 8, N_REG => 0) port map (VIN, RST_n, CLK, DIN_1, DIN_0_sig, C_1, DIN_1_sig);
+    stage_2 : MUL_ADD generic map (NBIT => 8, N_REG => 0) port map (VIN, RST_n, CLK, DIN_2, DIN_1_sig, C_2, DIN_2_sig);
+    stage_3 : MUL_ADD generic map (NBIT => 8, N_REG => 0) port map (VIN, RST_n, CLK, DIN_3, DIN_2_sig, C_3, DIN_3_sig);
+    stage_4 : MUL_ADD generic map (NBIT => 8, N_REG => 0) port map (VIN, RST_n, CLK, DIN_4, DIN_3_sig, C_4, DIN_4_sig);
+    stage_5 : MUL_ADD generic map (NBIT => 8, N_REG => 0) port map (VIN, RST_n, CLK, DIN_5, DIN_4_sig, C_5, DIN_5_sig);
+    reg_pipeline : REG port map (DIN_5_sig, CLK, RST_N, VIN_pipeline, DIN_5_sig_reg);
+    stage_6 : MUL_ADD generic map (NBIT => 8, N_REG => 1) port map (VIN, RST_n, CLK, DIN_6, DIN_5_sig_reg, C_6, DIN_6_sig);
+    stage_7 : MUL_ADD generic map (NBIT => 8, N_REG => 1) port map (VIN, RST_n, CLK, DIN_7, DIN_6_sig, C_7, DIN_7_sig);
+    stage_8 : MUL_ADD generic map (NBIT => 8, N_REG => 1) port map (VIN, RST_n, CLK, DIN_8, DIN_7_sig, C_8, DIN_8_sig);
+    stage_9 : MUL_ADD generic map (NBIT => 8, N_REG => 1) port map (VIN, RST_n, CLK, DIN_9, DIN_8_sig, C_9, DIN_9_sig);
+    stage_10 : MUL_ADD generic map (NBIT => 8, N_REG => 1) port map (VIN, RST_n, CLK, DIN_10, DIN_9_sig, C_10, DIN_10_sig);
 
     DOUT <= DIN_10_sig;
 
