7c2331499daff72b515619207caedf81944b4eae
Lorenzo Carrano <lorenzoc7297@gmail.com>
2021-12-06 00:29:16 +0100

    now inputs' lengths of stage 2 are correct

--- LAB2/MBE/dadda_adder_stage2.vhd
+++ LAB2/MBE/dadda_adder_stage2.vhd
@@ -7,16 +7,16 @@ Entity dadda_adder_stage2 IS
         Port(
             Partial_product_0 : IN std_logic_vector(34 DOWNTO 0);
             Partial_product_1 : IN std_logic_vector(35 DOWNTO 0);
-            Partial_product_2 : IN std_logic_vector(33 DOWNTO 0);
-            Partial_product_3 : IN std_logic_vector(31 DOWNTO 0);
-            Partial_product_4 : IN std_logic_vector(29 DOWNTO 0);
+            Partial_product_2 : IN std_logic_vector(30 DOWNTO 0);
+            Partial_product_3 : IN std_logic_vector(29 DOWNTO 0);
+            Partial_product_4 : IN std_logic_vector(11 DOWNTO 0);
             Partial_product_5 : IN std_logic_vector(28 DOWNTO 0);
             Partial_product_6 : IN std_logic_vector(28 DOWNTO 0);
             Partial_product_7 : IN std_logic_vector(28 DOWNTO 0);
             Partial_product_8 : IN std_logic_vector(28 DOWNTO 0);
             Partial_product_9 : IN std_logic_vector(28 DOWNTO 0);
             Partial_product_10: IN std_logic_vector(28 DOWNTO 0);
-            Partial_product_11: IN std_logic_vector(28 DOWNTO 0);
+            Partial_product_11: IN std_logic_vector(27 DOWNTO 0);
             Partial_product_12: IN std_logic_vector(25 DOWNTO 0);          
         );
 
