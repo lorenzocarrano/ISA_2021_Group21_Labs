10f7c9215e514370010c917903b21eac234d3774
Lorenzo Carrano <lorenzoc7297@gmail.com>
2021-12-06 00:24:30 +0100

    adjusted length of input operands in entity

--- LAB2/MBE/dadda_adder_stage2.vhd
+++ LAB2/MBE/dadda_adder_stage2.vhd
@@ -7,16 +7,16 @@ Entity dadda_adder_stage2 IS
         Port(
             Partial_product_0 : IN std_logic_vector(34 DOWNTO 0);
             Partial_product_1 : IN std_logic_vector(35 DOWNTO 0);
-            Partial_product_2 : IN std_logic_vector(30 DOWNTO 0);
-            Partial_product_3 : IN std_logic_vector(29 DOWNTO 0);
-            Partial_product_4 : IN std_logic_vector(11 DOWNTO 0);
+            Partial_product_2 : IN std_logic_vector(33 DOWNTO 0);
+            Partial_product_3 : IN std_logic_vector(31 DOWNTO 0);
+            Partial_product_4 : IN std_logic_vector(29 DOWNTO 0);
             Partial_product_5 : IN std_logic_vector(28 DOWNTO 0);
             Partial_product_6 : IN std_logic_vector(28 DOWNTO 0);
             Partial_product_7 : IN std_logic_vector(28 DOWNTO 0);
             Partial_product_8 : IN std_logic_vector(28 DOWNTO 0);
             Partial_product_9 : IN std_logic_vector(28 DOWNTO 0);
             Partial_product_10: IN std_logic_vector(28 DOWNTO 0);
-            Partial_product_11: IN std_logic_vector(27 DOWNTO 0);
+            Partial_product_11: IN std_logic_vector(28 DOWNTO 0);
             Partial_product_12: IN std_logic_vector(25 DOWNTO 0);          
         );
 
@@ -24,10 +24,25 @@ end dadda_adder_stage2;
 
 Architecture arch of dadda_adder_stage1 is
 
+    Component Addend_row is
 
+        Generic (N: integer:= 32);
 
+        Port( 
+            Addend_1       : IN     std_logic_vector (N-1 DOWNTO 0);
+            Addend_2       : IN     std_logic_vector (N-1 DOWNTO 0);
+            Addend_3       : IN     std_logic_vector (N-4 DOWNTO 0);
+            Sum            : OUT    std_logic_vector (N-1 DOWNTO 0);
+            Carry          : OUT    std_logic_vector (N-1 DOWNTO 0)  
+        );
+
+    end Component;
+    
+    SIGNAL op1, op2     : std_logic_vector(30 DOWNTO 0);
+    SIGNAL op3          : std_logic_vector(27 DOWNTO 0);
+    
     begin
 
-
+        op1(24 downto 0) <= Partial_product_0()
 
 end Architecture;
