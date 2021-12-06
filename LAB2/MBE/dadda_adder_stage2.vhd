9db23134e1a4da8f61b41ff2a54f26116ad29d4a
Lorenzo Carrano <lorenzoc7297@gmail.com>
2021-12-06 01:22:50 +0100

    operand3 assigned to partial products

--- LAB2/MBE/dadda_adder_stage2.vhd
+++ LAB2/MBE/dadda_adder_stage2.vhd
@@ -55,6 +55,14 @@ Architecture arch of dadda_adder_stage1 is
         op2(28 downto 27) <= Partial_product_6(28 downto 27);
         op2(30 downto 29) <= Partial_product_7(28 downto 27);
 
+        op3(3 downto 0) <= Partial_product_2(13 downto 0);
+        op3(4) <= Partial_product_3(12);
+        op3(20 downto 5) <= Partial_product_2(30 downto 15);
+        op3(21) <= Partial_product_3(29);
+        op3(23 downto 22) <= Partial_product_6(25 downto 24);
+        op3(24) <= Partial_product_7(24);
+        op3(26 downto 25) <= Partial_product_8(24 downto 23);
+        op3(26) <= Partial_product_9(23);
 
 
 end Architecture;
