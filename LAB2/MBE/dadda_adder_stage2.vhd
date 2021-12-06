a9d7e0613c4d73a28e74854853b9a40a324dfc69
Lorenzo Carrano <lorenzoc7297@gmail.com>
2021-12-06 02:42:07 +0100

    code alignment

--- LAB2/MBE/dadda_adder_stage2.vhd
+++ LAB2/MBE/dadda_adder_stage2.vhd
@@ -62,50 +62,50 @@ Architecture arch of dadda_adder_stage1 is
         op1(29 downto 30) <= Partial_product_6(28 downto 27);
 
         op2(24 downto 0)  <= Partial_product_1(34 downto 10);
-        op2(25) <= Partial_product_2(33);
-        op2(26) <= Partial_product_6(26);
+        op2(25)           <= Partial_product_2(33);
+        op2(26)           <= Partial_product_6(26);
         op2(28 downto 27) <= Partial_product_7(26 downto 25);
         op2(30 downto 29) <= Partial_product_8(26 downto 25);
         
-        op3(3 downto 0) <= Partial_product_2(13 downto 0);
-        op3(4) <= Partial_product_3(12);
-        op3(20 downto 5) <= Partial_product_2(30 downto 15);
-        op3(21) <= Partial_product_3(29);
+        op3(3 downto 0)   <= Partial_product_2(13 downto 0);
+        op3(4)            <= Partial_product_3(12);
+        op3(20 downto 5)  <= Partial_product_2(30 downto 15);
+        op3(21)           <= Partial_product_3(29);
         op3(23 downto 22) <= Partial_product_6(25 downto 24);
-        op3(24) <= Partial_product_7(24);
+        op3(24)           <= Partial_product_7(24);
         op3(26 downto 25) <= Partial_product_8(24 downto 23);
         op3(27) <= Partial_product_9(23);
 
         row1 : Addend_row GENERIC MAP (N => 31) PORT MAP (op1, op2, op3, somma1, carry1);
 
-        op4(3 downto 0) <= Partial_product_3(11 downto 8);
-        op4(4) <= Partial_product_4(10);
-        op4(5) <= Partial_product_3(13);
-        op4(6) <= Partial_product_5(10);
-        op4(20 downto 7) <= Partial_product_3(28 downto 15);
+        op4(3 downto 0)   <= Partial_product_3(11 downto 8);
+        op4(4)            <= Partial_product_4(10);
+        op4(5)            <= Partial_product_3(13);
+        op4(6)            <= Partial_product_5(10);
+        op4(20 downto 7)  <= Partial_product_3(28 downto 15);
         op4(23 downto 21) <= Partial_product_7(23 downto 21);
-        op4(24) <= Partial_product_8(22);
+        op4(24)           <= Partial_product_8(22);
         op4(26 downto 25) <= Partial_product_9(22 downto 21);
 
-        op5(3 downto 0) <= Partial_product_4(11 downto 8);
-        op5(4) <= Partial_product_5(10);
-        op5(5) <= Partial_product_4(13);
-        op5(6) <= Partial_product_6(10);
-        op5(20 downto 7) <= Partial_product_4(28 downto 15);
+        op5(3 downto 0)   <= Partial_product_4(11 downto 8);
+        op5(4)            <= Partial_product_5(10);
+        op5(5)            <= Partial_product_4(13);
+        op5(6)            <= Partial_product_6(10);
+        op5(20 downto 7)  <= Partial_product_4(28 downto 15);
         op5(23 downto 21) <= Partial_product_8(23 downto 21);
-        op5(24) <= Partial_product_9(22);
+        op5(24)           <= Partial_product_9(22);
         op5(26 downto 25) <= Partial_product_10(22 downto 21);
 
-        op6(1 downto 0) <= Partial_product_5(7 downto 0);
-        op6(2) <=  Partial_product_6(6);
-        op6(3) <= Partial_product_5(9);
-        op6(4) <= Partial_product_7(6);
-        op6(5) <= Partial_product_6(9);
-        op6(6) <= Partial_product_8(6);
-        op6(16 downto 7) <= Partial_product_6(20 downto 11);
-        op6(21 downto 8) <= Partial_product_9(19 downto 15);
-        op6(22) <= Partial_product_10(18);
-        op6(23) <= Partial_product_11(17);
+        op6(1 downto 0)   <= Partial_product_5(7 downto 0);
+        op6(2)            <=  Partial_product_6(6);
+        op6(3)            <= Partial_product_5(9);
+        op6(4)            <= Partial_product_7(6);
+        op6(5)            <= Partial_product_6(9);
+        op6(6)            <= Partial_product_8(6);
+        op6(16 downto 7)  <= Partial_product_6(20 downto 11);
+        op6(21 downto 8)  <= Partial_product_9(19 downto 15);
+        op6(22)           <= Partial_product_10(18);
+        op6(23)           <= Partial_product_11(17);
 
         row2 : Addend_row GENERIC MAP (N => 27) PORT MAP (op4, op5, op6, somma2, carry2);
 
