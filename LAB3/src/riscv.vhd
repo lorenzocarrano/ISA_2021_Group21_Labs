3ad5b0b4122e318637e2f4e5bdcc3f7e60f891f5
Dilillo Nicola <nicoladilillo98@gmail.com>
2021-12-14 10:59:01 +0100

    Add fetch part

--- LAB3/src/riscv.vhd
+++ LAB3/src/riscv.vhd
@@ -25,7 +25,41 @@ end risc_core;
 
 
 architecture ARCH of risc_core is
-
+    -- FETCH signals --
+    -- PC register that point the instruction adress target operation
+    PC, PCNext : std_logic_vector(N-1 DOWNTO 0);
+    -- For pipelined this stage
+    IF_ID, IF_IDNext : std_logic_vector(N-1 DOWNTO 0);
+    -- select how to calculate the PC adress value
+    Jump_PC std_logic:
 begin
 
+    Reg: process(clk)
+    begin
+        if(clk = '1')
+            if (rst = '1')
+                PC <= (Others => '0');
+                IF_ID <= (Others => '0');
+            else
+                PC <= PCNext;
+                IF_ID <= IF_IDNext;
+            end if;
+        end if;  
+    end process;
+
+    -- FETCH part
+    ADRESS_INSTRUCTION <= PC;
+    Fetch: process(PC, PC_mux, Jump_PC, DATA_INSTRUCTION)
+    begin
+        -- chose adress instruction memory 
+        case( PC_mux ) is
+            when 0 =>
+                PCNext <= PC + 4        
+            when others =>
+                PCNext <= Jump_PC
+        end case ;
+        
+        IF_IDNext <= DATA_INSTRUCTION;
+    end process Fetch;
+    
 end ARCH;
