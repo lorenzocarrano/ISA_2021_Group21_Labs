098c8f791ea12ba9c86501648c1dba7a4e02024a
Lorenzo Carrano <lorenzoc7297@gmail.com>
2022-02-06 17:19:29 +0100

    modified mux3to1.vhd in order to use a process in order to make its architecture behavioral instead of structural

--- LAB3/src/mux3to1.vhd
+++ LAB3/src/mux3to1.vhd
@@ -19,42 +19,18 @@ end mux3to1;
 
 Architecture Behavioral of mux3to1 is
 	
-	Component mux is
-	Generic
-	(
-		Nbit : natural := 1
-	);
-	Port
-	(
-		A    : in  std_logic_vector(Nbit-1 downto 0);
-		B    : in  std_logic_vector(Nbit-1 downto 0);
-		sel  : in  std_logic;
-		Y    : out std_logic_vector(Nbit-1 downto 0)
-	);
-	end Component;
-begin
-
-	mux1: mux Generic Map( Nbit => Nbit)
-		     Port Map
-		     (
-		     	A   => A,
-			B   => B,
-			sel => sel(0), 
-			Y   => ABselection
-		     );
-
-	mux2: mux Generic Map( Nbit => Nbit)
-		     Port Map
-		     (
-		     	A   => ABselection,
-			B   => C,
-			sel => sel(1),
-			Y   => Y
-		     );
-
---NOTE: input selection
---      A 00
---      B 01
---      C 1X
+	process(sel)
+	begin
+		if sel = "00" then 
+			Y <= A;
+		
+		else if sel = "01"
+			Y <= B;
+		
+		else 
+			Y <= C;
+		end if;
+	
+	end process;
 
 end Architecture;
