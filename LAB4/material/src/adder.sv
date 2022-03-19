d4bcb9281363563ea5b3e6bf4adb5e7364f51037
POLI VM <lorenzoc7297@gmail.com>
2022-03-19 12:42:25 +0100

    ok

--- LAB4/material/src/adder.sv
+++
@@ -1,6 +0,0 @@
-module adder(   input logic [31:0] A, B, 
-                output logic [31:0] OUT );
-
-    assign OUT = A + B;
-
-endmodule: adder
--- LAB4/material/src/dut_if.sv
+++
@@ -1,9 +0,0 @@
-interface dut_if(input clk, rst);
-    logic [31:0] A, B;
-    logic [31:0] data;
-    logic valid, ready;
-    
-    modport port_in (input clk, rst, A, B, valid, output ready);
-    modport port_out (input clk, rst, output valid, data, ready);
-endinterface
-
