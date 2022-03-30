687022f88d22a94e98a45e04665cb09f05ff9e05
POLI VM <lorenzoc7297@gmail.com>
2022-03-30 23:47:31 +0200

    simulations

--- LAB4/adder/tb/refmod.sv
+++ LAB4/adder/tb/refmod.sv
@@ -22,7 +22,7 @@ class refmod extends uvm_component;
         
         forever begin
             in.get(tr_in);
-            tr_out.data = tr_in.A + tr_in.B;
+            tr_out.data = tr_in.A - tr_in.B;
             $display("refmod: input A = %d, input B = %d, output OUT = %d",tr_in.A, tr_in.B, tr_out.data);
 			$display("refmod: input A = %b, input B = %b, output OUT = %b",tr_in.A, tr_in.B, tr_out.data);
             out.put(tr_out);
